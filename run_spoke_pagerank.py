3aca7a619417529466a42d96e1b21b81df269dd9
yjzhang <yjzhang@cs.washington.edu>
2022-09-13 17:47:33 -0700

    save results

--- run_spoke_pagerank.py
+++ run_spoke_pagerank.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import spoke_loader
 import pagerank_sparse
 
@@ -5,6 +7,7 @@ if __name__ == '__main__':
     nodes, edges, node_types, edge_types, edge_matrix = spoke_loader.load_spoke('spoke.csv')
     # run pagerank
     pr_probs_all = pagerank_sparse.pagerank(edge_matrix, modify_matrix=False, n_iters=50)
+    np.savetxt('pr_spoke.txt', pr_probs_all.flatten())
     pr_sorted = pr_probs_all.flatten().argsort()[::-1]
     top_pr_nodes = [nodes[i] for i in pr_sorted[:50]]
     # find interesting nodes?
@@ -16,6 +19,7 @@ if __name__ == '__main__':
     topics = [node_id_index[i] for i in topics]
     # run topic pagerank on t2d
     pr_probs_topic = pagerank_sparse.topic_pagerank(edge_matrix, topics, modify_matrix=False, resid=0.85, topic_prob=0.15, n_iters=50)
+    np.savetxt('pr_topics_spoke.txt', pr_probs_topic.flatten())
     topics_sorted = pr_probs_topic.flatten().argsort()[::-1]
     top_topic_genes = [nodes[i] + (pr_probs_topic[i],) for i in topics_sorted[:200] if nodes[i][2]==1]
     top_topic_compounds = [nodes[i] + (pr_probs_topic[i],) for i in topics_sorted[:1000] if nodes[i][2]==5]
@@ -24,7 +28,10 @@ if __name__ == '__main__':
     # try it with a symmetric adjacency matrix? are the results more reasonable?
     edge_matrix_symmetric = spoke_loader.symmetrize_matrix(edge_matrix)
     pr_probs_topic_symmetric = pagerank_sparse.topic_pagerank(edge_matrix_symmetric, topics, modify_matrix=False, resid=0.85, topic_prob=0.15, n_iters=50)
+    np.savetxt('pr_topics_spoke_symmetric.txt', pr_probs_topic_symmetric.flatten())
     topics_sorted = pr_probs_topic_symmetric.flatten().argsort()[::-1]
     top_topic_genes = [nodes[i] + (pr_probs_topic_symmetric[i],) for i in topics_sorted[:400] if nodes[i][2]==1]
     top_topic_compounds = [nodes[i] + (pr_probs_topic_symmetric[i],) for i in topics_sorted[:2000] if nodes[i][2]==5]
     top_topic_diseases = [nodes[i] + (pr_probs_topic_symmetric[i],) for i in topics_sorted[:200] if nodes[i][2]==9]
+
+    # save pagerank results?
